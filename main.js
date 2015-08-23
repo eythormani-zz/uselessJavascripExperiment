84849786ae1c2e43d93229da137e64a129657557
Eyþór Máni Steinarsson <eythormani@gmail.com>
2015-08-23 00:28:42 +0000

    Reyndi að bæta við collisions, gekk ekki

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ var ySpeed = (window.innerHeight-div.clientHeight)/50;//vantar minus width/heigh
 var xSpeed = (window.innerWidth-div.clientWidth)/50;
 var upPos = 0;
 var leftPos = 0;
-randomPosition();
+var stig = 0;
+moveThing();
 //eventlistener sem hlustar eftir því hvort ýtt sé á W, A, S eða D
 document.addEventListener("keypress", function(){
 	if (event.which == 119) {
@@ -63,8 +64,13 @@ function moveRight () {
 	leftPos = leftPos + xSpeed;
 	div.style.left = leftPos + "px";
 }
-
-function randomPosition () {
+function thing () {
+	//console.log('Works');
+	if (yEatLocation >= upPos) {
+		console.log('collided');
+	};
+}
+function moveThing () {
 	yEatLocation = cSharpHasABetterRandomNumberGenerator(0, (window.innerHeight - 60));
 	xEatLocation = cSharpHasABetterRandomNumberGenerator(0, (window.innerWidth - eat.clientWidth));
 	eat.style.top = yEatLocation + "px";
@@ -74,3 +80,6 @@ function randomPosition () {
 function cSharpHasABetterRandomNumberGenerator(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var gameLoop = setInterval(function(){
+	thing();
+}, 1000/60);
